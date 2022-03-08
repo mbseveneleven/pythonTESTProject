1e652187dad3668a83c83bd0f6ed1c5bd6a0c05a
Vladimir Gritsanyuk <mbseveneleven@gmail.com>
2022-03-08 19:36:32 +0700

    Semi-calculator

--- maintest.py
+++ maintest.py
@@ -1,4 +1,30 @@
 name = input("What is your name? ")
-print("Hello ", name)
-age = input("How old are you? ")
-print("Your age is a ", age)
+c = '+'
+if name == 'Vladimir':
+    print("Hello", name, "!")
+
+    a = input("Input value of 'a'!")
+    b = input("Input value of 'b'!")
+
+    while c == '+' or '-' or '*' or '/':
+        c = input("Input arithmetic sign, pls!")
+        if c == '+':
+            c = float(a) + float(b)
+            print("a + b = ", c)
+        elif c == '-':
+            c = float(a) - float(b)
+            print("a - b = ", c)
+        elif c == '*':
+            c = float(a) * float(b)
+            print("a * b = ", c)
+        elif c == '/':
+            c = float(a) / float(b)
+            print("a / b = ", c)
+        elif c == '0':
+            break
+        else:
+            print("Incorrect sign! For exit input 0")
+else:
+    exit(-1)
+
+print(c)
