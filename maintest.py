2f0029a52836a80608449a842808244bf07e7197
Vladimir Gritsanyuk <mbseveneleven@gmail.com>
2022-03-08 19:59:29 +0700

    Calculator without GUI

--- maintest.py
+++ maintest.py
@@ -1,30 +1,34 @@
 name = input("What is your name? ")
-c = '+'
+
 if name == 'Vladimir':
     print("Hello", name, "!")
 
-    a = input("Input value of 'a'!")
-    b = input("Input value of 'b'!")
-
-    while c == '+' or '-' or '*' or '/':
-        c = input("Input arithmetic sign, pls!")
-        if c == '+':
-            c = float(a) + float(b)
-            print("a + b = ", c)
-        elif c == '-':
-            c = float(a) - float(b)
-            print("a - b = ", c)
-        elif c == '*':
-            c = float(a) * float(b)
-            print("a * b = ", c)
-        elif c == '/':
-            c = float(a) / float(b)
-            print("a / b = ", c)
-        elif c == '0':
+    a = input("Input value of 'a'! ")
+    sign = '+'
+    while sign == '+' or '-' or '*' or '/':
+        sign = input("Input arithmetic sign, pls! For exit input '0' ")
+        if sign == '0':
             break
         else:
-            print("Incorrect sign! For exit input 0")
+            b = input("Input value of 'b'! ")
+            if sign == '+':
+                a = float(a) + float(b)
+                print("a + b = ", a)
+            elif sign == '-':
+                a = float(a) - float(b)
+                print("a - b = ", a)
+            elif sign == '*':
+                a = float(a) * float(b)
+                print("a * b = ", a)
+            elif sign == '/':
+                a = float(a) / float(b)
+                print("a / b = ", a)
+            else:
+                print("Incorrect sign! For exit input 0")
+
+
 else:
+    print("Wrong name!")
     exit(-1)
 
-print(c)
+print(a)
